d2b53f7ffec4a8ded5ff71de8b8bb3bf9a30ca32
Oskar Hoskuldsson <osh16@hi.is>
2021-03-15 22:12:19 +0000

    users.js skra buin til, sem ser um /users virkni

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 import express from 'express';
 import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
@@ -19,73 +20,9 @@ app.set('view engine', 'ejs');
 
 // app.use(express.static('src'));
 
-/*app.use('/', (req, res) => {
+app.use('/', (req, res) => {
   res.render('index');
 });
-*/
-
-// ====================
-
-const users = []; // tomt array til ad byrja med, thurfum ad bera saman vid gagnagrunn
-
-app.get('/users', (req, res) => {
-  res.json(users);
-});
-
-app.post('/register', async (req, res) => {
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  const newUser = { name: req.body.name, password: hashedPassword };
-  
-  // er notendanafnið laust?
-  const user = users.find(user => user.name = req.body.name);
-  if (user) {
-    return res.status(400).send("Notendanafn þegar í notkun");
-  }
-
-  // stofnum notanda
-  try {
-    // her tharf ad vera sql skipun sem stofnar notanda
-    users.push(newUser);
-    res.status(201).send("Notandi " + newUser.name + " búinn til");
-    // her kemur jwt token
-  } catch {
-    res.status(500).send("Eitthvað mistókst við nýskráningu");
-  }
-});
-
-app.post('/login', async (req, res) => {
-  // her tharf ad bera saman notenda vid gagnagrunn
-  const user = users.find(user => user.name = req.body.name);
-  if (user == null) {
-    console.log(user);
-    return res.status(400).send('Notandi fannst ekki');
-  }
-  try {
-    const loginCheck = await bcrypt.compare(req.body.password, user.password);
-    if (loginCheck) {
-      res.status(200).send("Notandi skráður inn");
-      // her kemur jwt token
-    } else {
-      res.status(500).send("Eitthvað mistókst við innskráningu");
-    }
-  } catch {
-    res.status(500).send();
-  }
-});
-
-app.post('/users', async (req, res) => {
-  try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
-      const user = { name: req.body.name, password: hashedPassword };
-      console.log(hashedPassword);
-      users.push(user);
-      res.status(201).send()
-  } catch {
-      res.status(500).send()
-  }
-});
-
-// ====================
 
 function notFoundHandler(req, res, next) {
   console.log('404', res);
---
+++ src/users.js
@@ -0,0 +1,104 @@
+/* 
+users.js
+Föll tengd notendaumsjón fara hingað t.d. login, register, o.s.frv.
+*/
+
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import express from 'express';
+import { query } from './setup.js';
+
+dotenv.config();
+
+const {
+    ACCESS_TOKEN_SECRET: jwtSecret,
+    ACCESS_TOKEN_LIFETIME: tokenLifetime = 20
+} = process.env;
+
+export const router = express.Router();
+
+const users = []; // tomt array til ad byrja med, thurfum ad bera saman vid gagnagrunn
+
+// er notendanafn til?
+function checkUsername(username) {
+    const checkUsername = users.find(user => user.name = req.body.name);
+    if (username) {
+        return false;
+    }
+    return true;
+}
+
+// er netfang til?
+function checkEmail(email) {
+    const email = users.find(user => user.email = req.body.email);
+    if (email) {
+        return false;
+    }
+    return true;
+}
+
+// athugar ef rett lykilorð var slegið inn
+async function comparePasswords(password, user) {
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if (checkPassword) {
+        return user;
+    }
+    return false;
+}
+
+// bua til notanda i gagnagrunn
+async function createUser(user) {
+    //const newUser = await query('INSERT INTO Users (name, email, password) VALUES ($1, $2, $3)');
+}
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.post('/users/register', async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const newUser = { name: req.body.name, email: req.body.email, password: hashedPassword };
+  
+  if (!checkUsername(user.name)) {
+    return res.status(400).send("Notendanafn þegar í notkun");
+  } 
+  
+  else if (!checkEmail(user.email)) {
+      return res.status(400).send("Netfang þegar í notkun");
+  }
+
+  try {
+    // ~~ her tharf ad vera sql skipun sem stofnar notanda ~~
+    //users.push(newUser);
+    //res.status(201).send("Notandi " + newUser.name + " búinn til");
+
+    // ~~ her aetti ad koma jwt token ~~
+    const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET);
+    res.json({ accessToken: accessToken });
+    
+  } catch {
+    res.status(500).send("Eitthvað mistókst við nýskráningu");
+  }
+});
+
+app.post('/users/login', async (req, res) => {
+    // her tharf ad bera saman notenda vid gagnagrunn
+    const user = users.find(user => user.name = req.body.name);
+    if (user == null) {
+        console.log(user);
+        return res.status(400).send('Notandi fannst ekki');
+    }
+    try {
+        const loginCheck = await bcrypt.compare(req.body.password, user.password);
+        if (loginCheck) {
+            //res.status(200).send("Notandi skráður inn");
+            // her kemur jwt token
+            const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET);
+            return res.status(200).json({ accessToken: accessToken });
+        } else {
+            return res.status(500).send("Eitthvað mistókst við innskráningu");
+        }
+    } catch {
+        return res.status(500).send("Eitthvað mistókst við innskráningu");
+    }
+});
