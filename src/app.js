08165145d5d90793301cd2c0a35ad87dbbc94ca7
Oskar Hoskuldsson <osh16@hi.is>
2021-03-16 18:37:40 +0000

    daldid ad slokkva elda, setja thad sem eg er kominn med

--- src/app.js
+++ src/app.js
@@ -6,39 +6,57 @@ import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
+import { router as userRouter } from './users.js';
+
+dotenv.config();
+
+const {
+  PORT: port = 3000,
+  ACCESS_TOKEN_SECRET: sessionSecret,
+} = process.env;
+
 const app = express();
 
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
 
-const path = dirname(fileURLToPath(import.meta.url));
+//const path = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(join(path, '../public')));
-
-app.set('views', join(path, '../views'));
-app.set('view engine', 'ejs');
+// app.use(express.static(join(path, '../public')));
+// 
+// app.set('views', join(path, '../views'));
+// app.set('view engine', 'ejs');
 
 // app.use(express.static('src'));
 
-app.use('/', (req, res) => {
-  res.render('index');
+// app.use('/', (req, res) => {
+//   res.render('index');
+// });
+
+
+app.get('/', (req, res) => {
+  res.json({'oskar':'ok'});
+  console.log(userRouter);
 });
 
+app.use('/users', userRouter);
+
 function notFoundHandler(req, res, next) {
-  console.log('404', res);
-  res.status(404).render('error', { title: 'Error', message: '404 Not Found' });
+  //console.log('404', res);
+  res.status(404).json({'error': { title: 'Error', message: '404 Not Found' }});
 }
 
 function errorHandler(error, req, res, next) {
-  console.log('500', res);
-  res.status(500).render('error', { title: 'Error', message: error });
+  //console.log('500', res);
+  res.status(500).json({'error':{ title: 'Error', message: error }});
 }
 
 app.use(notFoundHandler);
 app.use(errorHandler);
 
 const hostname = '127.0.0.1';
-const port = 3000;
+//const port = 3000;
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
--- src/users.js
+++ src/users.js
@@ -16,13 +16,14 @@ const {
 } = process.env;
 
 export const router = express.Router();
+//router.use(express.json());
 
 const users = []; // tomt array til ad byrja med, thurfum ad bera saman vid gagnagrunn
 
 // er notendanafn til?
 function checkUsername(username) {
     const checkUsername = users.find(user => user.name = req.body.name);
-    if (username) {
+    if (checkUsername) {
         return false;
     }
     return true;
@@ -30,8 +31,8 @@ function checkUsername(username) {
 
 // er netfang til?
 function checkEmail(email) {
-    const email = users.find(user => user.email = req.body.email);
-    if (email) {
+    const checkEmail = users.find(user => user.email = req.body.email);
+    if (checkEmail) {
         return false;
     }
     return true;
@@ -47,58 +48,58 @@ async function comparePasswords(password, user) {
 }
 
 // bua til notanda i gagnagrunn
-async function createUser(user) {
-    //const newUser = await query('INSERT INTO Users (name, email, password) VALUES ($1, $2, $3)');
-}
+//async function createUser(user) {
+//    //const newUser = await query('INSERT INTO Users (name, email, password) VALUES ($1, $2, $3)');
+//}
 
-app.get('/users', (req, res) => {
+router.get('/', (req, res) => {
   res.json(users);
 });
 
-app.post('/users/register', async (req, res) => {
+router.post('/register', async (req, res) => {
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
   const newUser = { name: req.body.name, email: req.body.email, password: hashedPassword };
   
-  if (!checkUsername(user.name)) {
-    return res.status(400).send("Notendanafn þegar í notkun");
+  if (!checkUsername(newUser.name)) {
+    return res.status(400).json({message: "Notendanafn þegar í notkun"});
   } 
   
-  else if (!checkEmail(user.email)) {
-      return res.status(400).send("Netfang þegar í notkun");
+  else if (!checkEmail(newUser.email)) {
+      return res.status(400).json({message: "Netfang þegar í notkun"});
   }
 
   try {
     // ~~ her tharf ad vera sql skipun sem stofnar notanda ~~
-    //users.push(newUser);
-    //res.status(201).send("Notandi " + newUser.name + " búinn til");
+    users.push(newUser);
+    res.status(201).json({message: "Notandi " + newUser.name + " búinn til"});
 
     // ~~ her aetti ad koma jwt token ~~
-    const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET);
-    res.json({ accessToken: accessToken });
+    //const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET);
+    //res.json({ accessToken: accessToken });
     
   } catch {
-    res.status(500).send("Eitthvað mistókst við nýskráningu");
+    res.status(500).json({message: "Eitthvað mistókst við nýskráningu"});
   }
 });
 
-app.post('/users/login', async (req, res) => {
+router.post('/login', async (req, res) => {
     // her tharf ad bera saman notenda vid gagnagrunn
     const user = users.find(user => user.name = req.body.name);
     if (user == null) {
         console.log(user);
-        return res.status(400).send('Notandi fannst ekki');
+        return res.status(400).json({message:'Notandi fannst ekki'});
     }
     try {
         const loginCheck = await bcrypt.compare(req.body.password, user.password);
         if (loginCheck) {
-            //res.status(200).send("Notandi skráður inn");
+            res.status(200).json({message:"Notandi skráður inn"});
             // her kemur jwt token
-            const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET);
-            return res.status(200).json({ accessToken: accessToken });
+            //const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET);
+            //return res.status(200).json({ accessToken: accessToken });
         } else {
-            return res.status(500).send("Eitthvað mistókst við innskráningu");
+            return res.status(500).json({message: "Eitthvað mistókst við innskráningu"});
         }
     } catch {
-        return res.status(500).send("Eitthvað mistókst við innskráningu");
+        return res.status(500).json({message:"Eitthvað mistókst við innskráningu"});
     }
 });
