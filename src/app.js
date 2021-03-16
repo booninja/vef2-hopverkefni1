0613b7b30ce10ed32eb74b519440930edb7fe181
RutB <42755206+RutB@users.noreply.github.com>
2021-03-16 18:51:39 +0000

    komin index síða og linkar fyrir allar síðurnar kommentað frá í index.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
+import {router as apiRouter} from './index.js';
 
 import express from 'express';
 import dotenv from 'dotenv';
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs');
 
 // app.use(express.static('src'));
 
+app.use('/', apiRouter);
 app.use('/', (req, res) => {
   res.render('index');
 });
@@ -41,3 +43,35 @@ const port = 3000;
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+/*
+tv":{"series":
+{"href":"/tv","methods":["GET","POST"]},"serie":{"href":"/tv/{id}","methods":["GET","PATCH","DELETE"]},
+"rate":{"href":"/tv/{id}/rate","methods":["POST","PATCH","DELETE"]},
+"state":{"href":"/tv/{id}/state","methods":["POST","PATCH","DELETE"]}},
+"seasons":{"seasons":{"href":"/tv/{id}/season","methods":["GET","POST"]},
+"season":{"href":"/tv/{id}/season/{season}","methods":["GET","DELETE"]}},
+"episodes":{"episodes":{"href":"/tv/{id}/season/{season}/episode","methods":["POST"]},
+"episode":{"href":"/tv/{id}/season/{season}/episode/{episode}","methods":["GET","DELETE"]}},
+"genres":{"genres":{"href":"/genres","methods":["GET","POST"]}},
+"users":{"users":{"href":"/users","methods":["GET"]},
+"user":{"href":"/users/{id}","methods":["GET","PATCH"]},
+"register":{"href":"/users/register","methods":["POST"]},
+"login":{"href":"/users/login","methods":["POST"]},
+"me":{"href":"/users/me","methods":["GET","PATCH"]}}}*/
+
+
+//not user
+  //getur séð öll gögn
+  //getur skráð sig inn og orðið auðkenndur notandi
+//user
+  //change status
+    //Langar að horfa
+    //er að horfa
+    //hef horft
+  //rate episode int 0 - 5
+//admin
+  //breytt
+  //bætt
+  // eytt      tv show, seasons, episodes
+  //admins can change users status
